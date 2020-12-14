8091c0fb30c5111ed260c1069db81cba1a9814b8
0z09e <62566206+0z09e@users.noreply.github.com>
2020-12-14 17:40:56 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 apt install guake
+pip3 install pyautogui
 cp shell_stabilizer.py $HOME/.stabilize
 chmod +x $HOME/.stabilize
 echo "alias stabilize=$HOME/.stabilize" >> $HOME/.bash_aliases
